27a35793c300b4dbf14447357f233fd2aed8a4ef
btmxh <ngoduyanh.chip@gmail.com>
2023-09-03 22:25:17 +0700

    feat: :sparkles: add basic server code

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+const port = parseInt(process.env.PORT ?? "36736");
+
+app.use(express.static("dist"));
+
+app.listen(port, () => {
+  console.log(`plst2 server running on http://localhost:${port}`);
+  console.log("Access the API via the /api/ endpoint");
+  console.log(
+    `Webapp frontend is available at http://localhost:${port}/index.html`
+  );
+});
